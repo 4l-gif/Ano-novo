edafa0ac6fe5bb95c3353386daad22ea731f0716
4l-gif <alinedscabral123@gmail.com>
2025-12-31 00:03:21 -0300

    Teste

---
+++ Entrenos.java
@@ -0,0 +1,10 @@
+public class Virada {
+
+    public static void main(String[] args) {
+
+        System.out.println("Voce sera uma das melhores partes do meu ano novo.");
+        System.out.println("Gosto de como voce chegou, sem forcar.");
+        System.out.println("Obrigado por estar aqui.");
+
+    }
+}
